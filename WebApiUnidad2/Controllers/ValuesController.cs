cc2e7940979e3bfbcb1251d180f5b000af2e8181
Robert Rozas Navarro <rorozasn@microsoft.com>
2019-08-23 11:57:50 -0400

    primera versión, capa de datos funcionando, también la injeccion de dependencias

---
+++ WebApiUnidad2/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApiUnidad2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static IConfiguration configuration;
+        public ValuesController(IConfiguration iconfiguration)
+        {
+            configuration = iconfiguration;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            DataTable ingredientes = new DataTable();
+            DataTable platillos = new DataTable();
+            Models.CapaDatos misDatos = null;
+
+            using (misDatos = new Models.CapaDatos(configuration))
+            {
+                ingredientes = await misDatos.getIngredientes();
+                platillos = await misDatos.getPlatillos();
+            }
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
